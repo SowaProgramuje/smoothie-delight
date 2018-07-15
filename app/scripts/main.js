26be15015ccf98c3831e8c23ed2dacd2b463e532
Olga <olga@sowaprogramuje.pl>
2018-07-16 00:42:21 +0200

    Add footer menu, add horizontal styles for usps component, add hamburger styles, style header on desktop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+const hamburgerIcon = document.querySelector('#hamburger');
+const header = document.querySelector('#header');
+
+(function toggleHamburger() {
+    console.log(hamburgerIcon);
+    hamburgerIcon.addEventListener('click', function() {
+        this.classList.toggle('hamburger--active');
+        header.toggle('header--active');
+    })
+})();
